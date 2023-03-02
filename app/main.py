4b583e22b3a7691cf2c7b7b43957e12bdc09d5f6
assassin_box <assassin543@outlook.jp>
2023-03-03 00:25:24 +0900

    画像生成コピペ

--- app/main.py
+++ app/main.py
@@ -1,5 +1,16 @@
+import io
+import os
+from PIL import Image
 from fastapi import FastAPI
-from stability_sdk import create_dog_image
+from fastapi.responses import FileResponse
+from stability_sdk import client
+import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
+
+# APIインタフェースの準備
+stability_api = client.StabilityInference(
+    key=os.environ['STABILITY_KEY'],
+    verbose=True,
+)
 
 app = FastAPI()
 
@@ -9,5 +20,15 @@ def read_root():
 
 @app.get("/dog_image")
 def generate_dog_image():
-    image_path = create_dog_image()
-    return FileResponse(image_path)
+    # テキストからの画像生成
+    answers = stability_api.generate(
+        prompt="white dog",
+    )
+    # 結果の出力
+    for resp in answers:
+        for artifact in resp.artifacts:
+            if artifact.finish_reason == generation.FILTER:
+                print("NSFW")
+            if artifact.type == generation.ARTIFACT_IMAGE:
+                img = Image.open(io.BytesIO(artifact.binary))
+                img.save('output.png')
