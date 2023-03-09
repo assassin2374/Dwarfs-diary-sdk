6dd08e10868a1e84e0b313ed638b066c8f4a253a
assassin_box <assassin543@outlook.jp>
2023-03-10 00:00:13 +0900

    生成されるファイル名をUUIDに適応

--- app/main.py
+++ app/main.py
@@ -1,29 +1,38 @@
 import io
 import os
+import uuid
 from PIL import Image
 from fastapi import FastAPI, Request
 from stability_sdk import client
 import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
 
-# APIインタフェースの準備
+# Stability APIの呼び出しのためのクライアントオブジェクトを作成する
 stability_api = client.StabilityInference(
-    key=os.environ['STABILITY_KEY'],
-    verbose=True,
+    key=os.environ['STABILITY_KEY'], # APIキーを環境変数から取得する
+    verbose=True, # ログ出力を有効にする
 )
 
+# FastAPIのアプリケーションオブジェクトを作成する
 app = FastAPI()
 
+# APIエンドポイントを定義する
 @app.get("/{prompt}")
-def generate_dog_image(prompt:str):
-    # テキストからの画像生成
-    answers = stability_api.generate(
-        prompt=prompt
-    )
-    # 結果の出力
+def generate_image(prompt:str):
+    # テキストからの画像生成を行う
+    answers = stability_api.generate(prompt=prompt)
+
+    # 生成された画像を保存するための処理を行う
     for resp in answers:
         for artifact in resp.artifacts:
+            # NSFW画像が生成された場合の処理
             if artifact.finish_reason == generation.FILTER:
                 print("NSFW")
+            # 生成された画像がPNGファイルの場合の処理
             if artifact.type == generation.ARTIFACT_IMAGE:
                 img = Image.open(io.BytesIO(artifact.binary))
-                img.save(prompt+".png")
+
+                # UUIDでファイル名を生成する
+                filename = str(uuid.uuid4()) + '.png'
+
+                # PILライブラリを使ってPNGファイルを保存する
+                img.save(filename +".png")
