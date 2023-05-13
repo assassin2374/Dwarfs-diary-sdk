59c3744118912285c89f5a6553035d28391bfd9a
assassin_box <assassin543@outlook.jp>
2023-05-13 23:51:07 +0900

    crud作成

---
+++ app/cruds/user.py
@@ -0,0 +1,14 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+import app.models.user as user_model
+import app.schemas.user as user_schema
+
+
+async def create_task(
+    db: AsyncSession, user_create: user_schema.UserCreate
+) -> user_model.User:
+    task = user_model.User(**user_create.dict())
+    db.add(task)
+    await db.commit()
+    await db.refresh(task)
+    return task
